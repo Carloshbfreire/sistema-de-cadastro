c80a56ad1f6aa282f8ad8a786e9b7e9ace662d3d
Carloshbfreire <carhb@outlook.com>
2023-06-16 17:09:00 -0300

    voltando para o estado do Paulo

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectendo ao banco de dados")
-// mais um console de testes:
-console.log ("O Paulo fez esse commit sem você ver na aula")
-console.log ("Enviando para o Github")
