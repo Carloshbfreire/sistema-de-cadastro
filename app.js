32cb436d11cba5eb9aea687d9572500ff4ac9913
Carloshbfreire <carhb@outlook.com>
2023-06-16 14:44:04 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectendo ao banco de dados")
+// mais um console de testes:
+console.log ("O Paulo fez esse commit sem você ver na aula")
+console.log ("Enviando para o Github")
